2bac42c235a1c625ff559647d12791b948c66441
Ian Caughley <Ian@StockSoftware.com.au>
2013-08-08 15:26:18 +1000

    Moved package config out of the rbenv recipe into the default.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,5 @@
+# xvfb is needed for some of our GUI tests
 package 'xvfb'
+
+# Postgres support
+package 'libpq-dev'
--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -22,7 +22,7 @@ end
 
 base_packages = %w{ tar bash curl git-core }
 cruby_packages = %w{ build-essential bison openssl libreadline6 libreadline6-dev
-        zlib1g zlib1g-dev libpq-dev libssl-dev libyaml-dev libsqlite3-0
+        zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0
         libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf
         libc6-dev ssl-cert subversion }
 jruby_packages =  %w{ make g++ }
